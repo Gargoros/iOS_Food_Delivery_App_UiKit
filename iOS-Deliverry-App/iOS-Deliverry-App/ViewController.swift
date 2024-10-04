dff84c1abb64ad05756f7339f44f6e53ebe77280
Gargoros <mikhailzhachko@gmail.com>
2024-10-04 13:48:38 +0300

    feat: created base project and removed storyboard references

---
+++ iOS-Deliverry-App/iOS-Deliverry-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Deliverry-App
+//
+//  Created by MIKHAIL ZHACHKO on 4.10.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .orange
+    }
+
+
+}
+
